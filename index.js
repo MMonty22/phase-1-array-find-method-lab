9de9db669761e5e0b57e0b5d392834f2ea1afaab
Michael <michael.c.montgomery@gmail.com>
2022-02-18 14:48:14 -0600

    complteted assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    const found = record.find(function(element){
+        return element.result === "W"
+    })
+return (found === undefined? undefined : found.year)
+};
